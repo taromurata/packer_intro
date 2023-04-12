7447ccbfee57b22d63155574978fac49aa949077
Taro Murata <taro@keio.jp>
2023-04-12 12:35:47 +0900

    Modify main.tf

--- 02_hcppacker_basics/terraform/use_packer_image/main.tf
+++ 02_hcppacker_basics/terraform/use_packer_image/main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "~> 0.56.0"
+    }
   }
 
   required_version = ">= 1.4.0"
@@ -13,8 +18,18 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+provider "hcp" {}
+
+data "hcp_packer_image" "packer-intro-ubuntu" {
+  bucket_name    = "packer-intro-ubuntu"
+  channel        = "latest"
+  cloud_provider = "aws"
+  region         = "ap-northeast-1"
+}
+
 resource "aws_instance" "db_server" {
-  ami           = "ami-072bfb8ae2c884cc4"
+  #  ami           = "ami-072bfb8ae2c884cc4"
+  ami           = data.hcp_packer_image.packer-intro-ubuntu.cloud_image_id
   instance_type = "t2.micro"
 
   tags = {
